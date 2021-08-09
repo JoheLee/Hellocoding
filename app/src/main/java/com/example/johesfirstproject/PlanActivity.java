61ea8e09b602972cf046258ab2b78ea8e10c7ff2
John Lee <junmyung123@gmail.com>
2021-08-08 20:29:04 -0500

    jp

--- app/src/main/java/com/example/johesfirstproject/PlanActivity.java
+++ app/src/main/java/com/example/johesfirstproject/PlanActivity.java
@@ -36,27 +36,20 @@ public class PlanActivity extends AppCompatActivity {
         String currentDate = new SimpleDateFormat("MM-dd-yyyy", Locale.getDefault()).format(currentTime);
         binding.date.setText(currentDate);
 
-
-        // initialisation with id's
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerview);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL));
-
+        // TODO: Start here
         ArrayList<String> source = AddItemsToRecyclerViewArrayList();
+        binding.date.setText(source.get(0));
+        binding.date.setText(source.get(1));
+        binding.date.setText(source.get(2));
+        binding.date.setText(source.get(3));
+        binding.date.setText(source.get(4));
+        binding.date.setText(source.get(5));
+        binding.date.setText(source.get(6));
 
-        Adapter adapter = new Adapter(source);
-        // Set Horizontal Layout Manager
-        // for Recycler view
-
-        // Set adapter on recycler view
-        recyclerView.setAdapter(adapter);
     }
 
-    // Function to add items in RecyclerView.
     public ArrayList<String> AddItemsToRecyclerViewArrayList()
     {
-        Date currentTime = Calendar.getInstance().getTime();
-
         // Adding items to ArrayList
         ArrayList<String> source = new ArrayList<>();
 
@@ -73,69 +66,6 @@ public class PlanActivity extends AppCompatActivity {
     private String getFormattedDate(int day) {
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DAY_OF_MONTH, day);
-        return new SimpleDateFormat("MM-dd-yyyy", Locale.getDefault()).format(calendar.getTime());
-    }
-
-    public class Adapter extends RecyclerView.Adapter<Adapter.MyView> {
-
-        // List with String type
-        private List<String> list;
-
-        // View Holder class which
-        // extends RecyclerView.ViewHolder
-        public class MyView extends RecyclerView.ViewHolder {
-
-            // Text View
-            TextView textView;
-
-            // parameterised constructor for View Holder class
-            // which takes the view as a parameter
-            public MyView(View view)
-            {
-                super(view);
-
-                // initialise TextView with id
-                textView = (TextView)view.findViewById(R.id.textview);
-            }
-        }
-
-        // Constructor for adapter class
-        // which takes a list of String type
-        public Adapter(List<String> horizontalList)
-        {
-            this.list = horizontalList;
-        }
-
-        // Override onCreateViewHolder which deals
-        // with the inflation of the card layout
-        // as an item for the RecyclerView.
-        @Override
-        public MyView onCreateViewHolder(ViewGroup parent, int viewType)
-        {
-            // Inflate item.xml using LayoutInflator
-            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
-
-            // return itemView
-            return new MyView(itemView);
-        }
-
-        // Override onBindViewHolder which deals
-        // with the setting of different data
-        // and methods related to clicks on
-        // particular items of the RecyclerView.
-        @Override
-        public void onBindViewHolder(final MyView holder, final int position) {
-
-            // Set the text of each item of
-            // Recycler view with the list items
-            holder.textView.setText(list.get(position));
-        }
-
-        // Override getItemCount which Returns
-        // the length of the RecyclerView.
-        @Override
-        public int getItemCount() {
-            return list.size();
-        }
+        return new SimpleDateFormat("dd/EEE", Locale.getDefault()).format(calendar.getTime());
     }
 }
