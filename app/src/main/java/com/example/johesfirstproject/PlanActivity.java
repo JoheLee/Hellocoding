49e94604d713382d231366b495ec6275a13ea5c1
John Lee <junmyung123@gmail.com>
2021-08-14 11:58:21 -0500

    ml

--- app/src/main/java/com/example/johesfirstproject/PlanActivity.java
+++ app/src/main/java/com/example/johesfirstproject/PlanActivity.java
@@ -1,23 +1,17 @@
 package com.example.johesfirstproject;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.johesfirstproject.databinding.PlanactivityBinding;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 public class PlanActivity extends AppCompatActivity {
@@ -37,24 +31,44 @@ public class PlanActivity extends AppCompatActivity {
         binding.date.setText(currentDate);
 
         // TODO: Start here
-        ArrayList<String> source = AddItemsToRecyclerViewArrayList();
-        binding.date.setText(source.get(0));
-        binding.date.setText(source.get(1));
-        binding.date.setText(source.get(2));
-        binding.date.setText(source.get(3));
-        binding.date.setText(source.get(4));
-        binding.date.setText(source.get(5));
-        binding.date.setText(source.get(6));
+        ArrayList<String> source = getNext7Days();
+        binding.date1.setText(source.get(0));
+        binding.date2.setText(source.get(1));
+        binding.date3.setText(source.get(2));
+        binding.date4.setText(source.get(3));
+        binding.date5.setText(source.get(4));
+        binding.date6.setText(source.get(5));
+        binding.date7.setText(source.get(6));
 
         binding.twelveam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Show Dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(PlanActivity.this);
+                builder.setMessage("From 01AM to 02AM");
+                // Get the layout inflater
+                LayoutInflater inflater = getLayoutInflater();
+                // Inflate and set the layout for the dialog
+                // Pass null as the parent view because its going in the dialog layout
+                builder.setView(inflater.inflate(R.layout.dialog, null))
+                        // Add action buttons
+                        .setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                // sign in the user ...
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                builder.create().show();
             }
         });
     }
 
-    public ArrayList<String> AddItemsToRecyclerViewArrayList()
+    public ArrayList<String> getNext7Days()
     {
         // Adding items to ArrayList
         ArrayList<String> source = new ArrayList<>();
